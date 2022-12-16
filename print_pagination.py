212a273f0503ebb43dfef38674c6bbe642e26395
Alek Ilchenko <yaichenko87@gmail.com>
2022-12-16 23:35:30 +0000

    Commit with pagination method and unittests for it

---
+++ print_pagination.py
@@ -0,0 +1,76 @@
+def print_pagination(current_page, total_pages, boundaries, around):
+    # Check exceptions
+    ## Is any input parametrs negative or incorrect?
+
+    if current_page < 1:
+        print('Current page should be at least 1 or bigger')
+        return
+
+    if total_pages < 1:
+        print('Total page should be at least 1 or bigger')
+        return
+
+    elif current_page > total_pages:
+        print('Current page cannot be bigget than total')
+        return
+    
+    ## Is boundaries negative or have incorrect value?
+    if boundaries < 0:
+        print('Boundaries cannot be negative')
+        return
+    elif boundaries > total_pages/2:
+        print('Boundaries cannot be more than half of total')
+        return
+
+    ## Is around negative or have incorrect value?
+    if around < 0:
+        print('Around cannot be negative')
+        return
+    
+    pages_to_link = []
+    count = 1 # Count was implemented to check all possible conditions of each pages 
+
+    # Add start boundaries
+    for i in range(1, boundaries+1):
+        pages_to_link.append(i)
+        count += 1
+
+    # Add hide link if count is outside range of current page BEFORE
+    if count < current_page - around:
+        pages_to_link.append('...')  
+
+    # Check that count not inside current page range and initionalize as a first page of range
+    if count not in range(current_page-around, current_page+around+1):
+        count = current_page-around
+    
+    # Add current page with his range and check that it isn't more than total or not in start boundaries
+    for i in range(count, current_page+around+1):
+        if i < total_pages and i not in range(1, boundaries+1):
+            pages_to_link.append(i)
+            count += 1
+    
+    # Add hide link if count is outside of current page range but don't achive boundaries
+    if count > current_page+around and count < total_pages-boundaries+1:
+        pages_to_link.append('...')
+    
+    # Check that count not in end boundaries range and initionalize as a first page of range
+    if count not in range(total_pages-boundaries+1, total_pages+1):
+        count = total_pages-boundaries+1
+
+    # Add end boudaries
+    for i in range(count, total_pages+1):
+        pages_to_link.append(i)
+    
+    # Print the pagination
+    print(' '.join([str(page) for page in pages_to_link]))
+
+if __name__ == '__main__':
+    # Example usage
+    print_pagination(1, 10, 2, 2)
+    print_pagination(5, 10, 2, 2)
+    print_pagination(8, 10, 2, 2)
+    print_pagination(10, 10, 2, 2)
+    print_pagination(1, 10, 5, 5)
+    print_pagination(100, 101, 3, 1)
+    print_pagination(39, 333, 3, 3)
+    print_pagination(1, 2, 1, 0)
---
+++ test_print_pagination.py
@@ -0,0 +1,87 @@
+import unittest
+from print_pagination import *
+import io
+from contextlib import redirect_stdout
+
+class TestPagination(unittest.TestCase):
+
+    def setUp(self):
+        # Create a new StringIO object before each test
+        self.buf = io.StringIO()
+
+    def test_current_page_too_low(self):
+        # Test current page less than 1
+        with redirect_stdout(self.buf):
+            print_pagination(0, 10, 2, 2)
+        output = self.buf.getvalue().strip()
+
+        # Compare the output to the expected result
+        self.assertEqual(output, "Current page should be at least 1 or bigger")
+
+    def test_current_page_too_high(self):
+        # Test current page greater than total pages
+        with redirect_stdout(self.buf):
+            print_pagination(11, 10, 2, 2)
+        output = self.buf.getvalue().strip()
+
+        # Compare the output to the expected result
+        self.assertEqual(output, "Current page cannot be bigget than total")
+
+    def test_total_pages_too_low(self):
+        # Test total pages less than 1
+        with redirect_stdout(self.buf):
+            print_pagination(1, 0, 2, 2)
+        output = self.buf.getvalue().strip()
+
+        # Compare the output to the expected result
+        self.assertEqual(output, "Total page should be at least 1 or bigger")
+
+    def test_boundaries_too_low(self):
+        # Test boundaries less than 0
+        with redirect_stdout(self.buf):
+            print_pagination(1, 10, -1, 2)
+        output = self.buf.getvalue().strip()
+
+        # Compare the output to the expected result
+        self.assertEqual(output, "Boundaries cannot be negative")
+
+    def test_boundaries_too_high(self):
+        # Test boundaries greater than half of total pages
+        with redirect_stdout(self.buf):
+            print_pagination(1, 10, 6, 2)
+        output = self.buf.getvalue().strip()
+
+        # Compare the output to the expected result
+        self.assertEqual(output, "Boundaries cannot be more than half of total")
+
+    def test_around_too_low(self):
+        # Test around less than 0
+        with redirect_stdout(self.buf):
+            print_pagination(1, 10, 2, -1)
+        output = self.buf.getvalue().strip()
+
+        # Compare the output to the expected result
+        self.assertEqual(output, "Around cannot be negative")
+
+    def test_pagination(self):
+        # Test pagination output with various parameters
+        with redirect_stdout(self.buf):
+            print_pagination(1, 10, 2, 2)
+            print_pagination(5, 10, 2, 2)
+            print_pagination(8, 10, 2, 2)
+            print_pagination(10, 10, 2, 2)
+        output = self.buf.getvalue().strip()
+        self.assertEqual(output, '1 2 3 ... 9 10\n1 2 3 4 5 6 7 ... 9 10\n1 2 ... 6 7 8 9 10\n1 2 ... 8 9 10')
+
+    def test_more_pagination(self):
+        with redirect_stdout(self.buf):
+            print_pagination(1, 10, 5, 5)
+            print_pagination(100, 101, 3, 1)
+            print_pagination(39, 333, 3, 3)
+            print_pagination(1, 2, 1, 0)
+        output = self.buf.getvalue().strip()
+        self.assertEqual(output, '1 2 3 4 5 6 7 8 9 10\n1 2 3 ... 99 100 101\n1 2 3 ... 36 37 38 39 40 41 42 ... 331 332 333\n1 2')
+
+
+if __name__ == '__main__':
+    unittest.main()
