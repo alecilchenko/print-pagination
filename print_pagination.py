9306c45e29a59ae2d36eab9737112f5f92ede636
Alex Ilchenko <alec.ilchenko@gmail.com>
2023-10-13 16:28:32 +0400

    Refactor names of variables

--- print_pagination.py
+++ print_pagination.py
@@ -7,21 +7,21 @@ def print_pagination(
 
     check_errors = {
         "Current page should be at least 1 or bigger.":
-            current_page >= 1,
+            current_page < 1,
         "Total page should be at least 1 or bigger.":
-            total_pages >= 1,
+            total_pages < 1,
         "Current page cannot be bigger than total.":
-            current_page <= total_pages,
+            current_page > total_pages,
         "Boundaries cannot be negative.":
-            boundaries >= 0,
+            boundaries < 0,
         "Around cannot be negative.":
-            around >= 0,
+            around < 0,
     }
-    if False in check_errors.values():
+    if True in check_errors.values():
         error_message = ""
-        for error, pass_status in check_errors.items():
-            if not pass_status:
-                error_message += error + "\n"
+        for message, error_status in check_errors.items():
+            if error_status:
+                error_message += message + "\n"
         raise ValueError(error_message)
 
     start = {
@@ -43,14 +43,14 @@ def print_pagination(
     all_pages = [page for page in range(1, total_pages + 1)]
 
     flag = True
-    count = 0
+    index = 0
     for number in all_pages:
         if number not in result and flag:
-            result.insert(count, "...")
+            result.insert(index, "...")
             flag = False
-            count += 1
+            index += 1
         if number in result:
             flag = True
-            count += 1
+            index += 1
     for i in result:
         print(i, end=" ")
